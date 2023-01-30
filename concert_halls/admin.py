bce2487728e53f6e7605417e0f6bf4831c349d9e
parkkuri <kebee19@naver.com>
2023-01-30 22:47:26 +0900

    init commit

--- concert_halls/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import ConcertHall, SeatArea
-
-admin.site.register(ConcertHall)
-admin.site.register(SeatArea)
--- concert_halls/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class ConcertHallsConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'concert_halls'
